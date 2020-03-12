ef611d4ffd828bff10e322cf5288e96464a582ac
Santi <santisanchezavalos@gmail.com>
2020-03-12 17:54:07 -0300

    Avances de estilo y escaneo

--- js/app.js
+++ js/app.js
@@ -1,25 +1,37 @@
 var app = new Vue({
     el: '#lausan-qr-escaner-web',
     data: {
-        urlEscaneada: ''
+        camaraPrendida: false,
+        spinnerActivo: false
     },
     methods: {
         grabar: function () {
+            var componente = this;
+            componente.camaraPrendida = true;
+            componente.spinnerActivo = true;
             const codeReader = new ZXing.BrowserQRCodeReader();
-            var dataComponente = this;
 
             codeReader
                 .listVideoInputDevices()
                 .then(videoInputDevices => {
-                    var idDispositivoSeleccionado = videoInputDevices.length > 1 ? videoInputDevices[1].deviceId : videoInputDevices[0].deviceId;
-                    codeReader
-                        .decodeOnceFromVideoDevice(idDispositivoSeleccionado, 'video')
-                        .then(result => {
-                            dataComponente.urlEscaneada = result.text;
-                        })
-                        .catch(err => console.error(err));
+                    if (videoInputDevices.length > 0) {
+                        var idDispositivoSeleccionado = videoInputDevices.length > 1 ? videoInputDevices[1].deviceId : videoInputDevices[0].deviceId;
+                        componente.spinnerActivo = false;
+                        codeReader
+                            .decodeOnceFromVideoDevice(idDispositivoSeleccionado, 'video')
+                            .then(result => {
+                                window.location.href = result.text;
+                            })
+                            .catch(() => {
+                                M.toast({ html: 'Error al escanear, por favor probá nuevamente' });
+                            });            
+                    } else {
+                        M.toast({ html: 'No se encontró ninguna cámara disponible' });
+                    }
                 })
-                .catch(err => console.error(err));
+                .catch(() => {
+                    M.toast({ html: 'Error al iniciar escaneo, por favor probá nuevamente' });
+                });            
         }
     }
 });
